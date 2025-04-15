795478b9610a65039864599291a91e45bd9d264a
JonathanM-A <kamajthomas@gmail.com>
2025-04-15 20:19:29 +0000

    Remove unneccesary files

--- apps/agents/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import AgentCreateView
-
-
-urlpatterns = [
-    path("agents/", AgentCreateView.as_view(), name="agent-create"),
-]
--- apps/external_tables/models.py
+++ apps/external_tables/models.py
@@ -9,23 +9,24 @@ from ..common.models import BaseModel
 from ..companies.models import Company
 from ..users.models import User
 from ..agents.models import Agent
+from ..customers.models import Customer, CustomerLoyaltyPoints
 
 
-class Customer(BaseModel):
-    user = models.ForeignKey(
-        User, on_delete=models.PROTECT, blank=False, related_name="user", null=True
-    )
-    loyalty_points = models.ManyToManyField(
-        Company,
-        through="CompanyLoyaltyPoints",
-        related_name="loyalty_points",
-        null=True,
-    )
-    tag = models.CharField(max_length=10, null=True)
+# class Customer(BaseModel):
+#     user = models.ForeignKey(
+#         User, on_delete=models.PROTECT, blank=False, related_name="user", null=True
+#     )
+#     loyalty_points = models.ManyToManyField(
+#         Company,
+#         through="CompanyLoyaltyPoints",
+#         related_name="loyalty_points",
+#         null=True,
+#     )
+#     tag = models.CharField(max_length=10, null=True)
 
-    class Meta:
-        managed = False
-        db_table = "customers"
+#     class Meta:
+#         managed = False
+#         db_table = "customers"
 
 
 class Transaction(BaseModel):
@@ -44,12 +45,12 @@ class Transaction(BaseModel):
         db_table = "transactions"
 
 
-class CompanyLoyaltyPoints(BaseModel):
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-    loyalty_points = models.IntegerField(validators=[MinLengthValidator(0)])
+# class CompanyLoyaltyPoints(BaseModel):
+#     company = models.ForeignKey(Company, on_delete=models.PROTECT)
+#     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+#     loyalty_points = models.IntegerField(validators=[MinLengthValidator(0)])
 
-    class Meta:
-        managed = False
-        db_table = "company_loyalty_points"
-        unique_together = (("company", "customer"),)
+#     class Meta:
+#         managed = False
+#         db_table = "company_loyalty_points"
+#         unique_together = (("company", "customer"),)
