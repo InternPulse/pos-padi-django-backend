c06b0e2bdab5039c01be0bc58795cda4b5d97531
voste126 <steveaustine126@gmail.com>
2025-04-18 19:31:04 +0300

    FEAT: added test cases to the user authentication process

--- config/settings/base.py
+++ config/settings/base.py
@@ -159,6 +159,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Ensure the STATICFILES_DIRS is defined to avoid warnings
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
